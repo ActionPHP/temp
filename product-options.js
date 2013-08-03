88ad4dc4775243490410b84418d89a9588fbbbb5
ActionPHP <giosis2e@yahoo.co.uk>
2013-08-03 15:00:29 +0100

    Update product-options.js

--- product-options.js
+++ product-options.js
@@ -1,5 +1,29 @@
 (function(){
 
+App.Models.Option = Backbone.Model.extend({
 
+
+});
+
+App.Models.OptionValue = Backbone.Model.extend({
+
+
+});
+
+
+/**
+
+Models:
+
+- Option
+- OptionValue
+
+Collections
+
+- Options
+- OptionValues
+
+
+*/
 
 })();
