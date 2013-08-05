5fca5c0e894f3225cb193c886cb85fdb0c405534
ActionPHP <giosis2e@yahoo.co.uk>
2013-08-05 17:22:49 +0100

    Update product-options.js

--- product-options.js
+++ product-options.js
@@ -15,6 +15,7 @@
 	<title>Product options</title>
 
 	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min.js" ></script>
 	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js"></script>
 	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
@@ -32,12 +33,12 @@
 	<!-- Templates -->
 
 	<script type="text/template" id="wpcart-product-option-template" >
-	<span id="wpcart-product-option-<%= id %>"><%= name %></span>
+	<span id="wpcart-product-option-<%= id %>" class="wpcart-product-option-name"><%= name %></span>
 	<ul class="wpcart-product-option-values"></ul>
 	</script>
 
 	<script type="text/template" id="wpcart-product-option-value-template" >
-	<li id="wpcart-product-option-value-<%= id %>"><%= value %></li>
+	<span id="wpcart-product-option-value-<%= id %>"><%= value %></span>
 	</script>
 
 	<script type="text/javascript">
@@ -88,6 +89,7 @@
 		initialize: function() {
 			
 			vent.on('product:options', this.render(), this);
+			vent.on('option-value-order:change', this.saveOptions, this);
 		},
 
 		render: function(){
@@ -129,10 +131,17 @@
 		addOption: function(option){
 
 			var optionView = new App.Views.Option({ model: option });
-			var _view = optionView.render().el;
-			console.log(_view);
-			this.$el.append(_view);
+			var _view = optionView.render();
+			
+			this.$el.append(_view.el);
+			var $el = _view.$el;
+			option.set('position', $el.index());
 
+		},
+
+		saveOptions: function(){
+
+			console.log(this.collection.toJSON());
 		}
 
 
@@ -154,102 +163,94 @@
 			var view = template(this.model.toJSON());
 			this.$el.html(view);
 
+
 			var optionValues = new App.Collections.OptionValues(this.model.get('options'));
 
-			
-			var optionValueView = new App.Views.OptionValue({collection: optionValues, el: $(this.$el).find('.wpcart-product-option-values')});
+			var optionValueList = $(this.$el).find('.wpcart-product-option-values');
+
+			$( optionValueList ).sortable({
+
+				update: function (event, ui) {
+					 console.log(ui);
+					vent.trigger('option-value-order:change');
+
+				}
+			});
+
+			var optionValueView = new App.Views.OptionValues({collection: optionValues, el: optionValueList });
 			
 			//This will also add the option values to the current view
 			var valuesView = optionValueView.render().el;
-			//this.$el.append(valuesView);
+			
+			this.$el.addClass("wpcart-product-option");
 
 			return this;
 		},
 
 		addOptionValue: function(value){
 
-			console.log(value)
+
 		}
 
 
 
 	});
 
-	App.Views.OptionValue = Backbone.View.extend({
-
-		//tagName: 'li',
-		elContainer: null,
+	App.Views.OptionValues = Backbone.View.extend({
 
 		initialize: function(){
 
-			console.log(this.$el);
 		},
 
 		render: function(){
 			var that = this;
-			var template = _.template($('#wpcart-product-option-value-template').html());
-			_.each(this.collection.models, function(optionValue){
-				console.log(optionValue.toJSON());
-				that.$el.append(template(optionValue.toJSON()));
 
-			});
-			//console.log(this.el);
+			_.each(this.collection.models, function(optionValue){
+				
+				var optionValueView = new App.Views.OptionValueItem({ model: optionValue });
+				var _view = optionValueView.render();
+				that.$el.append(_view.el);
+				$el = _view.$el;
+				optionValue.set('position', $el.index());
+				
+				});
 			return this;
 		},
 
 	});
 
-	/**
+	App.Views.OptionValueItem = Backbone.View.extend({
 
-	Models:
+		tagName: 'li',
 
-	- Option
-	- OptionValue
+		render: function() {
 
-	Collections
+			var template = _.template($('#wpcart-product-option-value-template').html());
+			var view = template(this.model.toJSON());
+			this.$el.html(view);
 
-	- Options
-	- OptionValues
+			return this;
 
-	Views
-
-	- OptionsSandbox ({ collection: Options }); //Contains all the options and their values
-	- Option({ collection: OptionValues }); // Contains the option and the values
-	- OptionValue //Contains a particular option value
-	*/
+		}
 
 
-	var options = [
-    {
-        "Size": {
-            "id": "1",
-            "name": "Size",
-            "options": [
-                {
-                    "id": "1",
-                    "value": "S",
-                    "position": "1"
-                },
-                {
-                    "id": "2",
-                    "value": "M",
-                    "position": "1"
-                },
-                {
-                    "id": "3",
-                    "value": "L",
-                    "position": "1"
-                }
-            ],
-            "position": "1"
-        }
-    }
-]
+	});
 
-//Let's start this thing
-//	
+	$( "#wpcart-product-options" ).sortable();
+
+	function orderOptions(){
+
+
+	}
+
+
+	//Let's start this thing
+		
 	new App.Views.OptionsSandbox;
 	vent.trigger('product:options');
+
+
+
 		})();
 	</script>
 </body>
